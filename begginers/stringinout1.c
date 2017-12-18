81e71e0cde0d655ba4c1a928937c4548e6484d64
SUGAPRIYAKUMARASAMY <34641604+SUGAPRIYAKUMARASAMY@users.noreply.github.com>
2017-12-18 15:28:17 +0530

    Create stringinout1.c

---
+++ begginers/stringinout1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	char str[100];
+	printf("enter the string:");
+    gets(str);
+    printf("enter the string:");
+    puts(str);
+	// your code goes here
+	return 0;
+}
