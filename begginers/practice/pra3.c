b85503f77723863bb72743bb85d3ff6d1ed3c236
SUGAPRIYAKUMARASAMY <34641604+SUGAPRIYAKUMARASAMY@users.noreply.github.com>
2017-12-18 21:44:42 +0530

    Create pra3.c

---
+++ begginers/practice/pra3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void)
+{
+char a;
+scanf("%c",&a);
+if(a=="a",a=="A",a=="e",a=="E",a=="i",a=="I",a=="o",a=="O",a=="u",a=="U")
+printf("vowel");
+else
+printf("consonant");
+return 0;
+}
