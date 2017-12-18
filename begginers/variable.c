307b33a919abcd1fc7f3ec24f53c8a3d2b7b14dd
SUGAPRIYAKUMARASAMY <34641604+SUGAPRIYAKUMARASAMY@users.noreply.github.com>
2017-12-18 14:31:45 +0530

    Create variable.c

---
+++ begginers/variable.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	char a[25]="sachin ramesh tendulkar";
+	char b[10]="little master";
+	int c=18426;
+	float d=44.36;
+	int e=463;
+	printf("%c\n%c\n%d\n%f\n%d\n",a,b,c,d,e);
+	return 0;
+}
