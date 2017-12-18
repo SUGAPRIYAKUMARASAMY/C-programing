d67ebc75603be2e14d8aed7013c2b3b5a2b1e385
SUGAPRIYAKUMARASAMY <34641604+SUGAPRIYAKUMARASAMY@users.noreply.github.com>
2017-12-18 16:35:34 +0530

    Create count.c

---
+++ players/count.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int count=1;
+int main(void) {
+	int count=0;
+	while(++count<5)
+	{
+		static int count=0;
+		++count;
+		printf("count=%d\n",count);
+	}
+		printf("count=%d\n",count);
+		print();
+	return 0;
+}
+void print()
+{
+	++count;
+		printf("count=%d\n",count);
+}
+
