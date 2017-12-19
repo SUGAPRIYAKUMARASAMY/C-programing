d7ad9b910cd4f5d20d7a185169382bb2eab7e44e
SUGAPRIYAKUMARASAMY <34641604+SUGAPRIYAKUMARASAMY@users.noreply.github.com>
2017-12-19 10:07:15 +0530

    Update pra3.c

--- begginers/practice/pra3.c
+++ begginers/practice/pra3.c
@@ -3,7 +3,7 @@ int main(void)
 {
 char a;
 scanf("%c",&a);
-if(a=="a",a=="A",a=="e",a=="E",a=="i",a=="I",a=="o",a=="O",a=="u",a=="U")
+if(a=='a'||a=='A'||a=='e'||a=='E'||a=='i'||a=='I'||a=='o'||a=='O'||a=='u'||a=='U')
 printf("vowel");
 else
 printf("consonant");
