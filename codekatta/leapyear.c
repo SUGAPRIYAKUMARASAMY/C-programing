e9d32d6d49134ce303f18e33a792f292214be832
SUGAPRIYAKUMARASAMY <34641604+SUGAPRIYAKUMARASAMY@users.noreply.github.com>
2018-03-07 17:40:43 +0530

    Create leapyear.c

---
+++ codekatta/leapyear.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(void)
+{
+	int a;
+	printf("\nenter a  number:");
+	scanf("%d",&a);
+	if(a%400==0)
+	{
+		printf("%d is leap year",a);
+	}
+		else if(a%100==0)
+	{
+	printf("%d is not a leap year",a);	
+	}
+	else if(a%4==0)
+	{
+			printf("%d is leap year",a);
+	}
+
+	else
+       printf("%d is not a leap year",a);
+       return 0;
+}
+
+
