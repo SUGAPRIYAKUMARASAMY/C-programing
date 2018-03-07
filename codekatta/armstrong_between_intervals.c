cf66bca0e977d90afb936c5cbd310005dc2a6fe1
SUGAPRIYAKUMARASAMY <34641604+SUGAPRIYAKUMARASAMY@users.noreply.github.com>
2018-03-07 18:04:26 +0530

    Create armstrong_between_intervals.c

---
+++ codekatta/armstrong_between_intervals.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(void)
+{
+	int low,upp,n,i;
+	scanf("%d%d",&low,&upp);
+	for(i=low;i<=upp;i++)
+	{
+		int rev=0,ams=0;
+		n=i;
+		while(n!=0)
+		{
+			rev=n%10;
+			ams=ams+(rev*rev*rev);
+			n=n/10;
+			
+		}
+		if(i==ams)
+		printf("%d\n",i);
+	}
+  return 0;
+}
