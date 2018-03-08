edf40ecf80b237dc42ab6d0098a7880561245350
SUGAPRIYAKUMARASAMY <34641604+SUGAPRIYAKUMARASAMY@users.noreply.github.com>
2018-03-08 10:37:15 +0530

    Create fibonacci.c

---
+++ codekatta/fibonacci.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() 
+
+{
+
+	int n,next,first=0,second=1,i;
+
+	scanf("%d",&n);
+
+	 for(i=0;i<n;i++)
+
+	{
+
+		if(n==1)
+
+		{
+
+			next=n;
+
+		}
+
+		else
+
+		{
+
+		first=second;
+
+		second=next;
+
+		next=first+second;
+
+		}
+
+	printf("%d ",next);
+
+	}
+
+return 0;
+
+}
