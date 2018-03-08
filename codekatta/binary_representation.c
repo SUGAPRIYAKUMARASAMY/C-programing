b9a735d7273add6b3ca0a827ed2d5b93a07d7635
SUGAPRIYAKUMARASAMY <34641604+SUGAPRIYAKUMARASAMY@users.noreply.github.com>
2018-03-08 17:13:42 +0530

    Create binary_representation.c

---
+++ codekatta/binary_representation.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) 
+{
+	char a[100];
+	int i,m,n;
+	scanf("%s",a);
+	m=strlen(a);
+	if(m==8)
+	{
+		
+		for(i=0;i<m;i++)
+			{
+				if(a[i]=='1'||a[i]=='0')
+				{
+				}
+				
+			}
+			printf("Yes");
+	}
+	else
+	printf("No");
+	return 0;
+}
